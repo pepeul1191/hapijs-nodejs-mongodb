72742736d5f4dfee8d76c1d379f009e9509e1bbe
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-01 11:45:10 -0500

    Actualizar un documento según _id

--- routes/blog.js
+++ routes/blog.js
@@ -21,9 +21,33 @@ module.exports = [
           author: author
         }
       );
-      blog.save(function (err, doc) {
+      blog.save(function (err, createdBlog) {
         if (err) return handleError(err);
-        reply(doc._id.toString());
+        reply(createdBlog._id.toString());
+      });
+    }
+  },
+  {
+    method: ['GET', 'POST'],
+    path: 'editar',
+    config: {
+      auth: false,
+      pre: [
+        { method: middleware.demo},
+      ],
+    },
+    handler: function (request, reply) {
+      var _id = request.query._id;
+      var title = request.query.title;
+      var author = request.query.author;
+      models.Blog.findById(_id,function(err, blog){
+        if (err) return handleError(err);
+        blog.title = title;
+        blog.author = author;
+        blog.save(function (err, updatedBlog) {
+          if (err) return handleError(err);
+          reply(JSON.stringify(updatedBlog));
+        });
       });
     }
   },
@@ -37,8 +61,8 @@ module.exports = [
       ],
     },
     handler: function (request, reply) {
-      models.Blog.find({},function(err, docs){
-        reply(JSON.stringify(docs));
+      models.Blog.find({},function(err, blogs){
+        reply(JSON.stringify(blogs));
       });
     }
   },
@@ -53,8 +77,8 @@ module.exports = [
     },
     handler: function (request, reply) {
       var _id = request.query._id;
-      models.Blog.findOne({_id: _id},function(err, doc){
-        reply(JSON.stringify(doc));
+      models.Blog.findOne({_id: _id},function(err, blog){
+        reply(JSON.stringify(blog));
       });
     }
   },
