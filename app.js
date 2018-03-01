1ca2ef79ad83749324d8a54da066156178fc0409
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-01 10:15:10 -0500

    Directorio de arhivos estáticos públicos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var models = require('./config/models');
 
 var blog = new models.Blog(
@@ -11,4 +12,55 @@ blog.save(function (err, doc) {
   if (err) return handleError(err);
   // saved!
   console.log(doc._id);
-})
+});
+*/
+
+'use strict';
+
+const Hapi = require('hapi');
+const Path = require('path');
+const Inert = require('inert');
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: 5000,
+  routes: {
+    cors: true,
+    files: {
+      relativeTo: Path.join(__dirname, 'public')
+    }
+  }
+});
+
+server.register(
+  [
+    Inert,
+  ], (err) => {
+  server.start((err) => {
+    console.log('Running web app at: ' + server.uri);
+  });
+});
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      index: true,
+    }
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  config: {
+    auth: false,
+  },
+  handler: function (request, reply) {
+    reply('Error: URL vacía');
+  }
+});
